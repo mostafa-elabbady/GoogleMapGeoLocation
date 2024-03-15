356126170d1f416f7512443e10f3e3dce8c62dc1
mosta <mosta@MOSTAFAPC30L2>
2024-03-15 05:34:58 -0500

    Add project files.

---
+++ GoogleMapGeoLocation/Program.cs
@@ -0,0 +1,45 @@
+﻿using GoogleMapGeoLocation;
+using Microsoft.SqlServer.Types;
+using Newtonsoft.Json;
+using System.Net;
+
+string apiKey = "GoogleAPIKey";
+
+List<Address> addresses = StaticMethods.GetAddressesFromDatabase();
+
+CancellationTokenSource source = new CancellationTokenSource();
+var options = new ParallelOptions { MaxDegreeOfParallelism = 10, CancellationToken = source.Token };
+try
+{
+    await Parallel.ForEachAsync(addresses, options, async (address, token) =>
+    {
+        // Create a WebClient to make the request
+        using (WebClient client = new WebClient())
+        {
+            string fullAddress = $"{address.AddressLine1}, {address.City}, {address.State} {address.ZipCode}";
+            string apiUrl = $"https://maps.googleapis.com/maps/api/geocode/json?address={WebUtility.UrlEncode(fullAddress)}&key={apiKey}";
+
+            string jsonResult = client.DownloadString(apiUrl);
+
+            dynamic result = JsonConvert.DeserializeObject(jsonResult);
+
+            double latitude = result.results[0].geometry.location.lat;
+            double longitude = result.results[0].geometry.location.lng;
+            address.Coordinates = SqlGeography.Point(latitude, longitude, 4326);
+            //Console.WriteLine($"Latitude: {latitude}");
+            //Console.WriteLine($"Longitude: {longitude}");
+            //Console.ReadLine();
+        }
+    });
+
+    foreach (Address address in addresses)
+    {
+        StaticMethods.UpdateCoordinatesInDatabase(address.Id, address.Coordinates);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
+
+
---
+++ GoogleMapGeoLocation/StaticMethods.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.SqlServer.Types;
+using Microsoft.Data.SqlClient;
+
+namespace GoogleMapGeoLocation;
+
+static class StaticMethods
+{
+    // Method to get addresses from the database
+    public static List<Address> GetAddressesFromDatabase()
+    {
+        List<Address> addresses = new List<Address>();
+        string connectionString = "Add Connection String here";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            string query = "SELECT Id, AddressLine1, City, State, Country, ZipCode FROM AddressTable";
+
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Address address = new Address
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            AddressLine1 = reader["AddressLine1"] == DBNull.Value ? null : (string)reader["AddressLine1"],
+                            City = reader["City"] == DBNull.Value ? null : (string)reader["City"],
+                            State = reader["State"] == DBNull.Value ? null : (string)reader["State"],
+                            Country = reader["Country"] == DBNull.Value ? null : (string)reader["Country"],
+                            ZipCode = reader["ZipCode"] == DBNull.Value ? null : (string)reader["ZipCode"],
+                            Coordinates = null
+                        };
+
+                        addresses.Add(address);
+                    }
+                }
+            }
+        }
+
+        return addresses;
+    }
+
+    // Method to update Coordinates in the database
+    public static void UpdateCoordinatesInDatabase(int Id, SqlGeography coordinates)
+    {
+        Console.WriteLine($"Updating Address Id {Id} and Coordinates {coordinates.STAsBinary()}");
+
+        string connectionString = "Add Connection String here";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            string query = "UPDATE AddressTable SET Coordinates = @Coordinates WHERE Id = @AddressId";
+
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Coordinates", coordinates.STAsBinary());
+                command.Parameters.AddWithValue("@AddressId", Id);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+}
